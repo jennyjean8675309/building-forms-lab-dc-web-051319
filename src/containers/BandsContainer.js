a6be8b30bb2f7db2aedd14670dd27bf0634cf406
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-08-07 13:50:05 -0400

    used mapStateToProps, mapDispatchToProps, and component state to render input from a form

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import { connect } from 'react-redux'
+import Band from '../components/Band'
 
 class BandsContainer extends Component {
+  renderBands = () => {
+    return this.props.bands.map( (band, index) => {
+      return <Band key={index} band={band} />
+    })
+  }
+
   render() {
     return(
       <div>
-        BandsContainer
+        <ul>
+          {this.renderBands()}
+        </ul>
+        <BandInput />
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => {
+  return {
+    bands: state.bands
+  }
+}
+
+export default connect(mapStateToProps)(BandsContainer)
